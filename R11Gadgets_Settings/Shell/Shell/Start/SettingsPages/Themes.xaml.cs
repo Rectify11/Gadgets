2e0b9bf336c813774c2449edbab1f3748e7dc7cd
Lixkote <95425619+Lixkote@users.noreply.github.com>
2023-09-02 06:42:28 +0200

    Add source code
    p

---
+++ R11Gadgets_Settings/Shell/Shell/Start/SettingsPages/Themes.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Shell.Shell.Start.SettingsPages
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class Themes : Page
+    {
+        public Themes()
+        {
+            this.InitializeComponent();
+            ToggleThemeTeachingTip1.IsOpen = true;
+        }
+    }
+}
